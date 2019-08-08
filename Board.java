f896f46295837595077fa91b3103185669f5315b
Jacob Collins <jacobacollins123@gmail.com>
2019-08-07 20:10:52 -0500

    Working on board.java

--- Board.java
+++ Board.java
@@ -6,36 +6,77 @@
 
 public class Board {
 
+    private int[][] board = new int[3][3];
+
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles){
-
+    board = tiles;
     }
 
     // string representation of this board
     public String toString(){
-        return "";
+        String boardString = "";
+
+        for(int [] x : board){
+            for(int y : x){
+                boardString += y + " ";
+            }
+            boardString += "\n";
+        }
+        return boardString;
     }
 
     // board dimension n
     public int dimension(){
-        return 0;
+        return board.length;
     }
 
     // number of tiles out of place
     public int hamming(){
-        return 0;
+        int counter = 0;
+        int [][] goalArray = board.clone();
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+               if((board[i][j] != i * board.length + j + 1)) counter++;
+            }
+        }
+        if(board[board.length-1][board.length-1] == 0) counter--;
+
+        return counter;
     }
 
     // sum of Manhattan distances between tiles and goal
     public int manhattan(){
-        return 0;
+
+        int manhattan = 0;
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if(board[i][j] == 0){
+                    continue;
+                }
+
+                manhattan += Math.abs((board[i][j] - 1)/board.length) + Math.abs(((board[i][j] - 1)%board.length) - j);
+            }
+        }
+        return manhattan;
     }
 
     // is this board the goal board?
     public boolean isGoal(){
-        return false;
+        // int [][] goalArray = board.clone();
+        // for(int i = 0; i < goalArray.length; i++){
+        //     for(int j = 0; j < goalArray[i].length; j++){
+        //         board[i][j] = i * goalArray.length + j + 1;
+        //     }
+        // }
+        // goalArray[goalArray.length-1][goalArray.length-1] = 0;
+
+        // return board == goalArray;
+    return hamming() == 0;
     }
+
+
 
     // does this board equal y?
     public boolean equals(Object y){
@@ -54,7 +95,13 @@ public class Board {
 
     // unit testing (not graded)
     public static void main(String[] args){
-        
+
     }
+     //   i
+     // j{0,0},{0,1},{0,2}     1,2,3    i * board.length + j + 1           for(int i = 0; i < board.length; i++)
+     //  {1,0},{1,1},{1,2}     4,5,6                                           for(int j = 0; j < board[i].length; j++)
+     //  {2,0},{2,1},{2,2}     7,8,9                                                   board[i][j] = i* board.length + j + 1
+     //                                                                    board[board.length-1][board.length - 1] = 0
+
 
 }
